09b03e6694ef7bb138e064e5e2298899f9b52858
Highscore <highscoretechng@gmail.com>
2023-06-15 17:42:18 +0100

    all games served

--- server.js
+++ server.js
@@ -1,36 +1,3 @@
-// const express = require('express')
-// // const mongoose = require('mongoose')
-
-
-
-// const cors = require('cors');
-
-// require('dotenv').config()
-// const app = express()
-
-// app.use(express.json())
-// app.use(cors())
-
-
-
-// app.get('/', ((req, res)=>{
-//     res.send("Server is running successfully")
-// }))
-
-// // mongoose.set('strictQuery', false);
-
-// // connect database
-
-// // const dbUri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@highscore.muku4gg.mongodb.net/hymnal?retryWrites=true&w=majority`;
-// // mongoose.connect(dbUri, { useNewUrlParser: true,  useUnifiedTopology: true })
-// //     .then((result)=>  console.log('Database connected'))
-// //     .catch((err)=> console.log(err))
-
-// app.listen(process.env.PORT, ()=>{
-//     console.log("Running on port "+ process.env.PORT)
-// })
-
-
 
 const express = require('express')
 const mongoose = require('mongoose')
