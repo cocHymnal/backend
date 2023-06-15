77a846278c5b64f2c52656956c520eb4d790dd40
Highscore <highscoretechng@gmail.com>
2023-06-15 16:21:55 +0100

    first commit

--- Server.js
+++ Server.js
@@ -1,9 +1,9 @@
 const express = require('express')
-const mongoose = require('mongoose')
+// const mongoose = require('mongoose')
 
-const profileRoutes = require('./routes/profileRoutes')
-const UserRoute = require('./routes/UsersRoutes')
-const AdminRoutes = require("./routes/AdminRoute")
+// const profileRoutes = require('./routes/profileRoutes')
+// const UserRoute = require('./routes/UsersRoutes')
+// const AdminRoutes = require("./routes/AdminRoute")
 
 const cors = require('cors');
 
@@ -13,22 +13,22 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-app.use('/api/users', UserRoute)
-app.use('/api/profile', profileRoutes)
-app.use('/api/admin', AdminRoutes)
+// app.use('/api/users', UserRoute)
+// app.use('/api/profile', profileRoutes)
+// app.use('/api/admin', AdminRoutes)
 
 app.get('/', ((req, res)=>{
     res.send("Server is running successfully")
 }))
 
-mongoose.set('strictQuery', false);
+// mongoose.set('strictQuery', false);
 
 // connect database
 
-const dbUri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@highscore.muku4gg.mongodb.net/hymnal?retryWrites=true&w=majority`;
-mongoose.connect(dbUri, { useNewUrlParser: true,  useUnifiedTopology: true })
-    .then((result)=>  console.log('Database connected'))
-    .catch((err)=> console.log(err))
+// const dbUri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@highscore.muku4gg.mongodb.net/hymnal?retryWrites=true&w=majority`;
+// mongoose.connect(dbUri, { useNewUrlParser: true,  useUnifiedTopology: true })
+//     .then((result)=>  console.log('Database connected'))
+//     .catch((err)=> console.log(err))
 
 app.listen(process.env.PORT, ()=>{
     console.log("Running on port "+ process.env.PORT)
